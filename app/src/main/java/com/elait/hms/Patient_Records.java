4f205fa32e0a76d61384f931a8b775efb2c87140
shreya gupta <shreyagupta134@yahoo.in>
2017-02-23 18:33:01 +0530

    Changes in MainActivity upto 23.02.2017 18:30 IST. Contains getter setter methods for History (records) table.

---
+++ app/src/main/java/com/elait/hms/Patient_Records.java
@@ -0,0 +1,55 @@
+package com.elait.hms;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by User on 2017-02-23.
+ */
+
+public class Patient_Records {
+    String Notes ,prescription, diagnosis, next_visit;
+
+    public String getNext_visit() {
+        return next_visit;
+    }
+
+    public void setNext_visit(String next_visit) {
+        this.next_visit = next_visit;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+
+        this.diagnosis = diagnosis;
+    }
+
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public String getDate(){
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date=new Date();
+        return simpleDateFormat.format(date);
+
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+}
+
